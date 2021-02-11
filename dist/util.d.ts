1bab8c6257c8922ff9b8a943dae00e81cadea0ac
Noah Gregory <nmgtheo@gmail.com>
2021-02-11 12:23:50 -0500

    built changes from last, broken, release

--- dist/util.d.ts
+++ dist/util.d.ts
@@ -11,6 +11,6 @@ declare type XORBase<T1, T2> = (T1 & {
 declare type XOR2<T1, T2> = XORBase<T1, T2>;
 declare type XOR3<T1, T2, T3> = XORBase<XORBase<T1, T2>, T3>;
 declare type HasAtLeastTwo<T extends {}[]> = T[0] & T[1] extends undefined ? false : true;
-declare type RemoveFirstTwo<T extends {}[]> = T extends [any, any, ...infer U] ? U : never;
+declare type RemoveFirstTwo<T extends {}[]> = T extends [{}, {}, ...infer U] ? U extends {}[] ? U : never : never;
 export declare type XOR<T extends {}[]> = HasAtLeastTwo<T> extends true ? HasAtLeastTwo<RemoveFirstTwo<T>> extends true ? XOR3<T[0], T[1], XOR<RemoveFirstTwo<T>>> : T[2] extends undefined ? XOR2<T[0], T[1]> : XOR3<T[0], T[1], T[2]> : never;
 export {};
